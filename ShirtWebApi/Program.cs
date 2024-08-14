eb6c69bb7cc52d3368bbc1ced63e28441f9c8e8b
azinmapar <azin.mapar@yahoo.com>
2024-08-14 13:58:50 +0330

    Routing added

--- ShirtWebApi/Program.cs
+++ ShirtWebApi/Program.cs
@@ -8,6 +8,33 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+//Routing
 
+// "/shirts"
+
+app.MapGet("/shirts", () =>
+{
+    return "showing all shirts";
+});
+
+app.MapGet("/shirts/{id}", (int id) =>
+{
+    return $"showing the shirt with id: {id}";
+});
+
+app.MapPost("/shirts", () =>
+{
+    return "adding a new shirt";
+});
+
+app.MapPut("/shirts/{id}", (int id) =>
+{
+    return $"updating the shirt with id: {id}";
+});
+
+app.MapDelete("/shirts/{id}", (int id) =>
+{
+    return $"deleting the shirt with id: {id}";
+});
 
 app.Run();
